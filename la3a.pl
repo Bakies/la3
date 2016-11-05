b97e76b526d9d72b136987005b48348d7488c33c
Jon Bakies <jon@baki.es>
2016-11-05 17:25:51 -0400

    Finished problem a

--- la3a.pl
+++ la3a.pl
@@ -3,20 +3,39 @@
 */
 
 /* insert your clauses here */
+/* Provide at least 6 facts and 4 rules and one conjunction in a rule. */
+good(luke).
+good(leia).
+good(obiwan).
+evil(emperor).
+evil(maul).
 
+hasLightsaber(luke).
+hasLightsaber(obiwan).
+hasLightsaber(emperor).
+hasLightsaber(maul).
 
-
-
-
-
-
-
+jedi(X) :- good(X), hasLightsaber(X).
+sith(X) :- evil(X), hasLightsaber(X).
 /* put your example queries in this comment under your clauses 
+?- [la3a].
+true.
 
+?- hasLightsaber(luke).
+true.
 
+?- jedi(X).
+X = luke ;
+X = obiwan.
 
+?- sith(X).
+X = emperor ;
+X = maul.
 
+?- sith(luke).
+false.
 
-
+?- jedi(leia).
+false.
 
 */
